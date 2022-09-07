91135b5ae457b434489678aec2cbb1c86d0000ea
ctmatias <ctmatias@ncsu.edu>
2022-09-06 23:26:20 -0400

    Started implementing walking animations

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,34 +4,46 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float playerSpeed = 5f;
 
-    // player speed
-    public float moveSpeed = 5f;
+    private Animator playerAnim;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        playerAnim = GetComponent<Animator>();
     }
 
-    // Update is called once per frame
-    void Update()
+    void MovePlayer()
     {
         float horizontalInput = Input.GetAxisRaw("Horizontal");
         float verticalInput = Input.GetAxisRaw("Vertical");
 
-        // player is moving to the left or right
+        // player is moving left or right
         if (horizontalInput != 0)
         {
-            Vector3 moveX = new Vector3(horizontalInput * moveSpeed * Time.deltaTime, 0f, 0f);
+            Vector3 moveX = new Vector3(horizontalInput * playerSpeed * Time.deltaTime, 0f, 0f);
             transform.Translate(moveX);
+            // determine if player is/was facing right
+            playerAnim.SetBool("isFacingRight", horizontalInput > 0);
         }
 
         // player is moving up or down
         if (verticalInput != 0)
         {
-            Vector3 moveY = new Vector3(0f, verticalInput * moveSpeed * Time.deltaTime, 0f);
+            Vector3 moveY = new Vector3(0f, verticalInput * playerSpeed * Time.deltaTime, 0f);
             transform.Translate(moveY);
         }
+
+        // initialize walking animation (x dir)
+        playerAnim.SetFloat("moveSpeedX", horizontalInput);
+        // initialize walking animation (y dir)
+        playerAnim.SetFloat("moveSpeedY", verticalInput);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        MovePlayer();
     }
 }
